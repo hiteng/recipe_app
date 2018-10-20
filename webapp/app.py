30e83afb347a5fa2fbe8484ae4877c0fbb22c21d
hiteng <hiteng@gmail.com>
2018-10-21 00:30:54 +1100

    Added Bootstrap,jQuery and updated node modules

--- webapp/app.py
+++ webapp/app.py
@@ -7,7 +7,11 @@ template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static"
 recipe_app = Flask(__name__, static_url_path='/static', static_folder='static', template_folder='static/templates')
 
 
+
 from recipe_app.webapp.blueprints.api_v1.recipe_views import api_v1
 
 recipe_app.register_blueprint(api_v1)
+
+from recipe_app.webapp import views
+
 
---
+++ webapp/views.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+from recipe_app.webapp.app import recipe_app
+from flask import render_template
+
+@recipe_app.route("/", methods=["GET"])
+def home():
+
+    return render_template("home.html")
